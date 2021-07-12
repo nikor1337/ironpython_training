022e2c48193fa81c3c3d7530ae0b9d939e50687e
nikor1337 <79450156+nikor1337@users.noreply.github.com>
2021-07-12 18:59:34 +0400

    init commit

---
+++ start.py
@@ -0,0 +1,6 @@
+import clr
+clr.AddReferenceByName('Microsoft.Office.Interop.Excel, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c')
+from Microsoft.Office.Interop import Excel
+
+excel = Excel.ApplicationClass()
+excel.Visible = True
